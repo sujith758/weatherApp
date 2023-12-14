3d999eaa133044f518164043ab13e1a7219fe998
Sujith <p.sujithk@gmail.com>
2023-12-14 07:46:51 +0530

    changes

---
+++ src/Components/Weather/weather.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./weather.css"
+
+const Weather = () => {
+  const [city, setCity] = useState("");
+  const changeHandler = (e) => {
+    setCity(e.target.value);
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(city);
+  };
+  return (
+    <div>
+      <center>
+        <div className="card">
+          <div className="card-body">
+            <h4 className="card-title">Weather App</h4>
+            <form onSubmit={submitHandler}>
+              <input
+                type="text"
+                name="city"
+                value={city}
+                onChange={changeHandler}
+              />
+              <br />
+              <br />
+              <input type="submit" value="Get Temperature" />
+            </form>
+          </div>
+        </div>
+      </center>
+    </div>
+  );
+};
+
+export default Weather;
